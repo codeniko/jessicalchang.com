d29020410f98474b3e4543dd823aaa24e1e0ae9b
Nikolay Feldman <niko@niko.rocks>
2018-03-03 14:53:49 -0800

    push2prod to push2master since no longer using gh pages

--- push2master.sh
+++ push2master.sh
@@ -3,7 +3,7 @@
 git checkout master && git pull origin dev && git push origin master && git checkout dev
 
 if [ $? -eq 0 ]; then
-  echo "Successfully pushed to prod."
+  echo "Successfully pushed to master."
 else
-  echo "Failed to push to prod."
+  echo "Failed to push to master."
 fi
